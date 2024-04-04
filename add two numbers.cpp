653330999bebc95c6b2f99165d65a0d95601909d
26APR2004 <159209658+26APR2004@users.noreply.github.com>
2024-04-04 14:22:41 +0530

    Add files via upload

---
+++ add two numbers.cpp
@@ -0,0 +1,15 @@
+//addition of two numbers
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int  a,b;
+	cout<<"Enter the a and b:"<<endl;
+	cin>>a>>b;
+
+	int sum=a+b;//formula
+
+	cout<<"addition of a and b:"<<sum<<endl;
+	return 0;
+}
