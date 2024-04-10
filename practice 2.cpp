c42a164687da59ba7c9c81c7a0042433353ae817
26APR2004 <159209658+26APR2004@users.noreply.github.com>
2024-04-10 14:32:48 +0530

    Add files via upload

---
+++ practice 2.cpp
@@ -0,0 +1,11 @@
+//Print the sum of two numbers
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"Enter the two numbers:"<<endl;
+	cin>>a>>b;
+	int sum=a+b;
+	cout<<"sum of"<<a<<"and"<<b<<"is:"<<sum<<endl;
+}
